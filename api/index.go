eb4f79ba18e0d3768aea5267d8d3272b5f388494
mbraeutig <m.braeutigam@posteo.de>
2020-03-26 19:34:25 +0100

    ...

--- api/index.go
+++ api/index.go
@@ -8,9 +8,25 @@ import (
 // Handler ...
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Url:%s", r.URL.Path)
+	if r.URL.Path == "/api/edit" {
+		editHandler(w, r)
+	}
+	if r.URL.Path == "/api/view" {
+		viewHandler(w, r)
+	}
+	if r.URL.Path == "/api/save" {
+		saveHandler(w, r)
+	}
 }
 
-// func main() {
-// 	http.HandleFunc("/", Handler)
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "editHandler")
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "viewHandler")
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "saveHandler")
+}

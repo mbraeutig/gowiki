3c3437a436384c062c07f7b71422bd7321a64bf3
mbraeutig <m.braeutigam@posteo.de>
2020-03-26 20:21:38 +0100

    ...

--- api/index.go
+++ api/index.go
@@ -12,23 +12,23 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Handling url: ", r.URL.Path)
 
-	if r.URL.Path == "/api/edit" {
-		editHandler(w, r)
-	}
-	if r.URL.Path == "/api/view" {
+	if r.URL.Path == "/api/view/" {
 		viewHandler(w, r)
 	}
-	if r.URL.Path == "/api/save" {
+	if r.URL.Path == "/api/edit/" {
+		editHandler(w, r)
+	}
+	if r.URL.Path == "/api/save/" {
 		saveHandler(w, r)
 	}
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "editHandler")
-}
-
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "viewHandler")
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "editHandler")
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {

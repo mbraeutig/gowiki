c4fb14424b856f9a430014cac7eb4f134541ce2b
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-31 22:54:59 +0200

    more stuff

--- api/index.go
+++ api/index.go
@@ -2,8 +2,28 @@ package handler
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 // Handler ...
 func Handler(w http.ResponseWriter, r *http.Request) {

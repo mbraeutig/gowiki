41e7c3c5b41cba0456e97ccf6b413e76801a53a0
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-31 23:02:13 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -6,6 +6,25 @@ import (
 	"net/http"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 // Handler ...
 func Handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/api/view/" {
@@ -29,25 +48,6 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "saveHandler")
-}
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
 }
 
 func init() {
